3c14358553da47d685d2b36b0d769bcce9fc68f0
thangnvhe <137912183+thangnvhe@users.noreply.github.com>
2023-07-04 15:23:06 +0700

    Update Brand.java

--- src/Brand.java
+++ src/Brand.java
@@ -1,4 +1,61 @@
 
+package Classes;
+
+
 public class Brand {
+    String brandID;
+    String brandName;
+    String soundBrand;
+    double price;
+
+    public Brand() {
+    }
+
+    public Brand(String brandID, String brandName, String soundBrand, double price) {
+        this.brandID = brandID;
+        this.brandName = brandName;
+        this.soundBrand = soundBrand;
+        this.price = price;
+    }
+
+    public String getBrandID() {
+        return brandID;
+    }
+
+    public void setBrandID(String brandID) {
+        this.brandID = brandID;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getSoundBrand() {
+        return soundBrand;
+    }
+
+    public void setSoundBrand(String soundBrand) {
+        this.soundBrand = soundBrand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "brandID=" + brandID + ", brandName=" + brandName + ", soundBrand=" + soundBrand + ", price=" + price;
+    }
+    
     
 }
+
+
